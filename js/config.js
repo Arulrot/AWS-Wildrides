320427370f36e7262413b0c4bf528cc3285fb808
Arulmohan P <117821665+Arulrot@users.noreply.github.com>
2025-03-31 10:52:02 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://c48zf8xvaf.execute-api.ap-south-1.amazonaws.com/dep' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://c48zf8xvaf.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
