c21b801c1d9a068fc4dbb0918456e037f939e513
Arulmohan P <117821665+Arulrot@users.noreply.github.com>
2025-03-31 09:34:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_OYwpxXvkd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3hacvh9j4qet57u10l0bt8h5jn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
